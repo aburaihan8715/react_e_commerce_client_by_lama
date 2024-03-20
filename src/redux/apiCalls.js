c82ed80cc1c41990d6e9c5bfde61eee8e6f510f6
aburahan8721 <aburaihan8721@gmail.com>
2024-03-20 15:45:20 +0600

    some chnages

--- src/redux/apiCalls.js
+++ src/redux/apiCalls.js
@@ -4,7 +4,7 @@ import { publicRequest } from '../requestMethods';
 export const login = async (dispatch, user) => {
   dispatch(loginStart());
   try {
-    const res = await publicRequest.post('/auth/login', user);
+    const res = await publicRequest.post('auth/login', user);
     // console.log(res);
     dispatch(loginSuccess(res.data.data));
   } catch (err) {
